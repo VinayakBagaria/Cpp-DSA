d301225dabf3d6623d39f9b4d197e9094c65db14
Vinayak Bagaria <workvinayak95@gmail.com>
2025-06-18 10:07:45 +0530

    Leetcode 72 - Medium - DP Bottom Up

--- dp/leetcode-72-medium.cpp
+++ dp/leetcode-72-medium.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 class Solution {
 private:
+    void print(vector<vector<int>> result) {
+        for (auto row : result) {
+            for (auto e : row) {
+                cout << e << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
     int solve(int i, int j, string& word1, string& word2, vector<vector<int>>& dp) {
         if (j == word2.size()) {
             return word1.size() - i;
@@ -34,6 +44,25 @@ private:
     }
 
 public:
+    int bottomUp(string word1, string word2) {
+        int w1 = word1.size(), w2 = word2.size();
+        vector<vector<int>> dp(w1 + 1, vector<int>(w2 + 1, 0));
+        for (int i = 0; i <= w1; i++) dp[i][0] = i;
+        for (int j = 0; j <= w2; j++) dp[0][j] = j;
+
+        for (int i = 1; i <= w1; i++) {
+            for (int j = 1; j <= w2; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[w1][w2];
+    }
+
     int minDistance(string word1, string word2) {
         vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
         return solve(0, 0, word1, word2, dp);
@@ -44,6 +73,11 @@ public:
 void doWork() {
     Solution sol;
     cout << sol.minDistance("horse", "ros") << endl;
+    cout << sol.bottomUp("horse", "ros") << endl;
+
     cout << sol.minDistance("intention", "execution") << endl;
+    cout << sol.bottomUp("intention", "execution") << endl;
+
     cout << sol.minDistance("", "a") << endl;
+    cout << sol.bottomUp("", "a") << endl;
 }
