42b5baf61bc4cff991454cc128d4932ba67ced68
Vinayak Bagaria <workvinayak95@gmail.com>
2025-08-02 01:20:03 +0530

    DP - 62 - Unique Paths - Medium - Bottom Up

--- dp/leetcode-62-medium.cpp
+++ dp/leetcode-62-medium.cpp
@@ -18,10 +18,27 @@ public:
         vector<vector<int>> dp(m, vector<int>(n, -1));
         return solve(0, 0, m, n, dp);
     }
+
+    int bottomUp(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int r = 0; r < m; r++) dp[r][n-1] = 1;
+        for (int c = 0; c < n; c++) dp[m-1][c] = 1;
+
+        for (int r = m-2; r >= 0; r--) {
+            for (int c = n-2; c >= 0; c--) {
+                dp[r][c] = dp[r + 1][c] + dp[r][c + 1];
+            }
+        }
+
+        return dp[0][0];
+    }
 };
 
 void doWork() {
     Solution sol;
     cout << sol.uniquePaths(3, 7) << endl;
+    cout << sol.bottomUp(3, 7) << endl;
     cout << sol.uniquePaths(3, 2) << endl;
+    cout << sol.bottomUp(3, 2) << endl;
 }
