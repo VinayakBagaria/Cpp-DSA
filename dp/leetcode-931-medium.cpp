dfffdbfefcbadafebb0cebafb76a6e4cd4b2eaec
Vinayak Bagaria <workvinayak95@gmail.com>
2025-08-02 03:35:12 +0530

    DP - 931 - Minimum Falling Path Sum - Medium - Bottom up Space optimized

--- dp/leetcode-931-medium.cpp
+++ dp/leetcode-931-medium.cpp
@@ -46,6 +46,31 @@ public:
 
         return *min_element(dp[0].begin(), dp[0].end());
     }
+
+    int spaceBottomUp(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> prev(n), curr(n);
+        
+        for (int r = m-1; r >= 0; r--) {
+            for (int c = n-1; c >= 0; c--) {
+                if (r == m-1) {
+                    curr[c] = matrix[r][c];
+                } else {
+                    int mini = prev[c];
+                    if (c >= 1) {
+                        mini = min(mini, prev[c-1]);
+                    }
+                    if (c + 1 < n) {
+                        mini = min(mini, prev[c+1]);
+                    }
+                    curr[c] = matrix[r][c] + mini;
+                }
+            }
+            prev = curr;
+        }
+
+        return *min_element(prev.begin(), prev.end());
+    }
 };
 
 void doWork() {
@@ -53,7 +78,9 @@ void doWork() {
     vector<vector<int>> vec = {{2, 1, 3}, {6, 5, 4}, {7, 8, 9}};
     cout << sol.minFallingPathSum(vec) << endl;
     cout << sol.bottomUp(vec) << endl;
+    cout << sol.spaceBottomUp(vec) << endl;
     vec = {{-19, 57}, {-40, -5}};
     cout << sol.minFallingPathSum(vec) << endl;
     cout << sol.bottomUp(vec) << endl;
+    cout << sol.spaceBottomUp(vec) << endl;
 }
