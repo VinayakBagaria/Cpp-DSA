ad535b32059091ad989727f36e8ed0067b41a9b8
Vinayak Bagaria <workvinayak95@gmail.com>
2025-08-03 14:54:46 +0530

    DP - 322. Coin Change - Medium - Bottom Up

--- dp/leetcode-322-medium.cpp
+++ dp/leetcode-322-medium.cpp
@@ -30,10 +30,39 @@ public:
         int result = solve(coins, amount, coins.size() - 1, dp);
         return result == 1e9 ? -1 : result;
     }
+
+    int bottomUp(vector<int>& coins, int amount) {
+        if (amount == 0) return 0;
+        
+        sort(coins.begin(), coins.end());
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 1e9));
+
+        for (int a = 0; a <= amount; a++) {
+            dp[0][a] = (a % coins[0] == 0) ? a / coins[0] : 1e9;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int a = 0; a <= amount; a++) {
+                int take = 1e9;
+                if (coins[i] <= a) {
+                    take = 1 + dp[i][a - coins[i]];
+                }
+                int notTake = dp[i-1][a];
+                dp[i][a] = min(take, notTake);
+            }
+        }
+
+        return dp[n-1][amount] == 1e9 ? -1 : dp[n-1][amount];
+    }
 };
 
 void doWork() {
     Solution sol;
     vector<int> vec = {1,2,5};
     cout << sol.coinChange(vec, 11) << endl;
+    cout << sol.bottomUp(vec, 11) << endl;
+    vec = {1, 2};
+    cout << sol.coinChange(vec, 2) << endl;
+    cout << sol.bottomUp(vec, 2) << endl;
 }
