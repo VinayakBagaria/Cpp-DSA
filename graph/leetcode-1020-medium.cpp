fbb6eda3c71311986f91315a6e7caf47101a54d4
Vinayak Bagaria <workvinayak95@gmail.com>
2025-08-11 22:19:14 +0530

    1020. Number of Enclaves - Graph - DFS simplified using multi-source

--- graph/leetcode-1020-medium.cpp
+++ graph/leetcode-1020-medium.cpp
@@ -7,38 +7,34 @@ class Solution {
 public:
     vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    int dfs(int i, int j, vector<vector<int>>& grid) {
+    void dfs(int i, int j, vector<vector<int>>& grid) {
         int r = grid.size(), c = grid[0].size();
-        if (i < 0 || i >= r || j < 0 || j >= c) return 1e9;
-        if (grid[i][j] == 0) return 0;
+        if (i < 0 || i >= r || j < 0 || j >= c || grid[i][j] == 0) return;
 
         grid[i][j] = 0;
-        int sum = 1;
-        bool isFalsy = false;
 
         for (auto& d : directions) {
-            int child = dfs(i + d[0], j + d[1], grid);
-            if (child == 1e9) isFalsy = true;
-            else sum += child;
+            dfs(i + d[0], j + d[1], grid);
         }
-
-        return isFalsy ? 1e9 : sum;
     }
 
     int numEnclavesDfs(vector<vector<int>>& grid) {
         int r = grid.size(), c = grid[0].size();
-        int result = 0;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 1) {
-                    int count = dfs(i, j, grid);
-                    if (count != 1e9) {
-                        result += count;
-                    }
+                if ((i == 0 || i == r - 1 || j == 0 || j == c - 1) && grid[i][j] == 1) {
+                    dfs(i, j, grid);
                 }
             }
         }
-        return result;
+
+        int count = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1) count++; 
+            }
+        }
+        return count;
     }
 
     // Multi-source BFS
@@ -82,7 +78,7 @@ public:
     }
 
     int numEnclaves(vector<vector<int>>& grid) {
-        return numEnclavesBfs(grid);
+        return numEnclavesDfs(grid);
     }
 };
 
